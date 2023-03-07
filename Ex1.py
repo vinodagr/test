8ec00f374f572fe57bb96711b1107ed958291b2b
vinodagr <111001042+vinodagr@users.noreply.github.com>
2023-03-07 23:23:36 +0530

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -30,7 +30,7 @@ def app():
  with st.form(key='placement-form'):
 # Get input from user
 # Add a text input widget
-  tenth = st.text_input('Enter your 10th percentage')
+  tenth = int(st.text_input('Enter your 10th percentage'))
   twelth = st.text_input('Enter your 12th percentage')
   UG = st.text_input('Enter your UG percentage')
   PG = st.text_input('Enter your PG percentage')
@@ -38,7 +38,7 @@ def app():
   UG_Course = st.text_input('Enter UG specialization 1-BCA  2-BCS  3-B.Com: ')
 
 # Create input dataframe
-input_df = pd.DataFrame({tenth:['tenth'],'twelth': [twelth], 'UG': [UG],'PG': [PG], 'Gender': [Gender],'UG_Course': [UG_Course]})
+input_df = pd.DataFrame({'tenth':[tenth],'twelth': [twelth], 'UG': [UG],'PG': [PG], 'Gender': [Gender],'UG_Course': [UG_Course]})
 
 
 
