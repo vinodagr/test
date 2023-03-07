93f23c6e5ce2f27fb76ef90f79d8e4d0af67c109
vinodagr <111001042+vinodagr@users.noreply.github.com>
2023-03-07 23:25:04 +0530

    Update Ex1.py

--- Ex1.py
+++ Ex1.py
@@ -30,7 +30,7 @@ def app():
  with st.form(key='placement-form'):
 # Get input from user
 # Add a text input widget
-tenth=0;
+  tenth=0;
   tenth = st.text_input('Enter your 10th percentage')
   twelth = st.text_input('Enter your 12th percentage')
   UG = st.text_input('Enter your UG percentage')
